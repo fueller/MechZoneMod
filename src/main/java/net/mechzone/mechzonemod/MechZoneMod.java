6bd8cdc75a100ec01cfcb9077e7e55d52af84941
fueller <philiplawall@gmail.com>
2014-07-06 22:07:06 +0200

    first commit

---
+++ src/main/java/net/mechzone/mechzonemod/MechZoneMod.java
@@ -0,0 +1,28 @@
+package net.mechzone.mechzonemod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "mechzonemod", name = "MechZoneMod", version = "1.7.10-1.0")
+public class MechZoneMod {
+
+    @Mod.Instance("MechZoneMod")
+    public static MechZoneMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
